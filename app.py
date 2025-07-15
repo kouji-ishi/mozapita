a9d559f52f8237ec7c0deaf9c40e6cf2d6a0acfd
石渡 浩二 <k-ishiwata@bbbank.jp>
2025-07-15 16:59:33 +0900

    fix: set host and port for Render deployment

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def download_selected():
     return send_file(zip_base_name + '.zip', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
